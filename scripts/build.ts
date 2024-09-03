704567afd0a569c5c4d3c03aa03be0644799d5ef
towelsh <tea@towel.sh>
2024-09-03 19:16:23 +0100

    add build vars

--- scripts/build.ts
+++ scripts/build.ts
@@ -2,6 +2,7 @@ import * as sass from 'sass';
 import fs from 'fs/promises';
 import { minify } from "minify";
 import { parse } from 'node-html-parser';
+import vars from "./build_vars";
 
 const IN = "./src";
 const OUT = "./build";
@@ -11,23 +12,33 @@ const start = Date.now();
 
 type HtmlProcessOptions = {
     styles?: string[],
+    vars?: Record<string, string>
 }
 const processHtml = async (html: string, options?: HtmlProcessOptions) => {
+    console.log("Processing HTML file");
     const root = parse(html);
     const head = root.querySelector("head");
     if (head == null) {
-        console.error("Attempted to process HTML without head tag");
+        console.error("-! Attempted to process HTML without head tag");
         return root.toString();
     }
 
     if (options?.styles) {
         for (const style of options.styles) {
             head.appendChild(parse("<link rel=\"stylesheet\" href=\"" + style + "\" />"));
-            console.log("appended link: " + "<link rel=\"stylesheet\" href=\"" + style + "\" />");
+            console.log("- appended link: " + "<link rel=\"stylesheet\" href=\"" + style + "\" />");
         }
     }
 
-    return root.toString();
+    let str = root.toString();
+    if (options?.vars) {
+        for (const [key, value] of Object.entries(options.vars)) {
+            console.debug("- setting {{ " + key + " }} to " + value);
+            str = str.replaceAll("{{ " + key + " }}", value);
+        }
+    }
+
+    return str;
 }
 
 /**
@@ -37,7 +48,7 @@ const processHtml = async (html: string, options?: HtmlProcessOptions) => {
  * @param base base directory of output (for calculating relative paths)
  */
 const processStyles = async (dir: string, out: string, baseOut: string) => {
-    console.log("processing styles in " + dir);
+    console.log("Processing styles in " + dir);
     let styles: string[] = [];
 
     // ensure the output direcotry exists
@@ -46,7 +57,7 @@ const processStyles = async (dir: string, out: string, baseOut: string) => {
         await fs.rm(out, { recursive: true, force: true });
     }
     await fs.mkdir(out, { recursive: true });
-    console.log("creating output dir: " + out);
+    console.log("- creating output dir: " + out);
 
     const input = await fs.readdir(dir, { withFileTypes: true });
     for (const file of input) {
@@ -58,22 +69,22 @@ const processStyles = async (dir: string, out: string, baseOut: string) => {
         const name = file.name.split(".");
         const ext = "." + name.pop();
         if (ext !== ".scss") {
-            console.warn("Non sass file will not be processed in style dir: " + file.name);
+            console.warn("- Non sass file will not be processed in style dir: " + file.name);
             continue;
         }
 
         const compiled = await sass.compile(dir + "/" + name + ext);
-        const mini = await minify.css(compiled.css);
-        console.log("writing file: " + out + "/" + name + ".min.css");
-        await fs.writeFile(out + "/" + name + ".min.css", mini);
+        // const mini = await minify.css(compiled.css);
+        console.log("- writing file: " + out + "/" + name + ".min.css");
+        await fs.writeFile(out + "/" + name + ".min.css", compiled.css);
         styles.push(out.substring(baseOut.length) + "/" + name + ".min.css");
-        console.log("dbg: " + file.name + " -> " + out.substring(baseOut.length) + "/" + name + ".min.css");
+        console.debug("- dbg: " + file.name + " -> " + out.substring(baseOut.length) + "/" + name + ".min.css");
     }
     return styles;
 }
 
-const build = async (dir: string, out: string, inheritedStyles?: string[]) => {
-    console.log("building " + dir);
+const build = async (dir: string, out: string, vars: Record<string, string>, inheritedStyles?: string[]) => {
+    console.log("Building from " + dir);
    
     if (await fs.exists(out)) {
         // output folder already exists
@@ -99,7 +110,7 @@ const build = async (dir: string, out: string, inheritedStyles?: string[]) => {
                 // this directory is processed above, we don't need to do it again
                 continue;
             }
-            await build(dir + "/" + file.name, out + "/" + file.name, styles);
+            await build(dir + "/" + file.name, out + "/" + file.name, vars, styles);
         } else {
             const name = file.name.split(".");
             const ext = "."+name.pop();
@@ -107,11 +118,15 @@ const build = async (dir: string, out: string, inheritedStyles?: string[]) => {
                 case ".scss":
                     console.warn("Sass files should be placed under a style directory, buliding anyway: " + file.name);
                     const compiled = await sass.compile(dir + "/" + name + ext);
-                    const mini = await minify.css(compiled.css);
-                    await fs.writeFile(out + "/" + name+".min.css", mini);
+                    // const mini = await minify.css(compiled.css);
+                    await fs.writeFile(out + "/" + name+".min.css", compiled.css);
                     break;
                 case ".html":
-                    const processed = await processHtml(await fs.readFile(dir + "/" + name + ext, "utf-8"), { styles: styles });
+                    const processed = await processHtml(await fs.readFile(dir + "/" + name + ext, "utf-8"),
+                        {
+                            styles: styles,
+                            vars
+                        });
                     await fs.writeFile(out + "/" + name + ext, processed);
                     break;
                 default:
@@ -122,7 +137,7 @@ const build = async (dir: string, out: string, inheritedStyles?: string[]) => {
     }
 }
 
-await build(IN, OUT);
+await build(IN, OUT, vars());
 console.log("Built in " + (Date.now() - start) + "ms");
 
 if (process.argv[2] === "serve" || process.argv[2] === "watch") {
@@ -139,7 +154,7 @@ if (process.argv[2] === "serve" || process.argv[2] === "watch") {
                 const start = Date.now();
                 console.log(event);
                 try {
-                    await build(IN, OUT);
+                    await build(IN, OUT, vars());
                     console.log("Built in " + (Date.now() - start) + "ms");
                 } catch(e) {
                     console.error("Build failed: " + e);
---
+++ scripts/build_vars.ts
@@ -0,0 +1,9 @@
+import { execSync } from "child_process";
+export default (): Record<string, string> => {
+    return {
+        "BASE_GITHUB": "towelsh/site",
+        "BUILD_TIME": Date.now().toString(),
+        "COMMIT_HASH": execSync("git rev-parse --short HEAD").toString().trim(),
+        "COMMIT_HASH_FULL": execSync("git rev-parse HEAD").toString().trim(),
+    };
+}
