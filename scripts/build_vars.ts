5fdc1207db571b94f325d67e16aa1f766e0b33c7
towelsh <tea@towel.sh>
2024-11-05 18:35:20 +0000

    update domain

--- scripts/build_vars.ts
+++ scripts/build_vars.ts
@@ -1,7 +1,7 @@
 import { execSync } from "child_process";
 export default (): Record<string, string> => {
     return {
-        "BASE_GITHUB": "towelsh/site",
+        "BASE_GITHUB": "towelcodes/site",
         "BUILD_TIME": Date.now().toString(),
         "COMMIT_HASH": execSync("git rev-parse --short HEAD").toString().trim(),
         "COMMIT_HASH_FULL": execSync("git rev-parse HEAD").toString().trim(),
