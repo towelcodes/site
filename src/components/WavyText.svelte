d5f374facdd502cf8f290b187df6279c8cc614ee
towelsh <tea@towel.sh>
2025-01-15 20:52:15 +0000

    big changes etc

---
+++ src/components/WavyText.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    import {onMount} from "svelte";
+
+    let { title }: { title: string} = $props();
+    let componentRoot: HTMLElement;
+
+    onMount(() => {
+        componentRoot.querySelectorAll("span").forEach((c, i) => {
+            c.style.animationDelay = (-200 * i) + "ms";
+        });
+    })
+</script>
+
+<h1 class="block text-7xl font-jersey font-bold text-transparent bg-clip-text mx-4" bind:this={componentRoot}>
+    {#each title.split('') as char}
+        <span class="bg-clip-text bg-gradient-to-r from-pink to-mauve
+      duration-500 ease-out cursor-default animate-wave inline-block drop-shadow-glow">{char}</span>
+    {/each}
+</h1>
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,9 +2,26 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		fontFamily: {
+			'jersey': ['"Jersey 10"', '"MS UI Gothic"', 'sans-serif'],
+			"sans": ["'MS UI Gothic'", "nec_apc3", "Verdana", "sans-serif"]
+		},
 		extend: {
 			boxShadow: {
-				"card": "5px 5px 0px rgba(0, 0, 0, 0.2)"
+				"card": "5px 5px 0px rgba(0, 0, 0, 0.2)",
+				"glow": "0px 0px 30px 0px #d7b0ff, inset 0px 0px 30px 20px rgba(241, 165, 255, 0.4)"
+			},
+			dropShadow: {
+				"glow": "0 0 10px #9836d9",
+			},
+			animation: {
+				"wave": "wave 1s cubic-bezier(.6,.01,.47,.99) infinite alternate"
+			},
+			keyframes: {
+				"wave": {
+					"0%": { transform: "translateY(-10px)" },
+					"100%": { transform: "translateY(10px)" }
+				}
 			}
 		},
 	},
