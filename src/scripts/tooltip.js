dc5af70beee9bbe7fb8afcd1bd6a5ab713314d00
towelsh <tea@towel.sh>
2025-02-04 21:18:35 +0000

    make the pointy thing a bit better

--- src/scripts/tooltip.js
+++ src/scripts/tooltip.js
@@ -15,16 +15,17 @@ const hover = (text) => {
 
 const show = () => {
     base.animate({
-        maxWidth: "1000px"
-    }, { duration: 1000, fill: "forwards"});
+        clipPath: "inset(0 0% 0 0)"
+    }, { duration: 300, fill: "forwards", easing: "ease-out"});
 }
 
 const hide = () => {
     base.animate({
-        maxWidth: "0px"
-    }, { duration: 1000, fill: "forwards"});
+        clipPath: "inset(0 100% 0 0)"
+    }, { duration: 300, fill: "forwards", easing: "ease-in"});
 };
 
+// FIXME this needs to be called each time page data changes
 document.querySelectorAll("[data-tooltip]").forEach((e) => {
     e.addEventListener("mouseover", () => hover(e.dataset.tooltip));
     e.addEventListener("mouseleave", hide);
