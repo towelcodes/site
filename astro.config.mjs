7fc12b737c0a59a64d646b6a757b95217c542066
towelsh <tea@towel.sh>
2025-02-05 23:24:32 +0000

    display commit message

--- astro.config.mjs
+++ astro.config.mjs
@@ -16,6 +16,7 @@ export default defineConfig({
       BASE_GITHUB: envField.string({ context: "client", access: "public", default: "https://github.com/towelcodes/site" }),
       COMMIT_HASH: envField.string({ context: "client", access: "public", default: "unknown" }),
       COMMIT_TIME: envField.number({ context: "client", access: "public", default: 0 }),
+      COMMIT_MSG: envField.string({ context: "client", access: "public", default: "no message"}),
     }
   }
 });
--- scripts/env.js
+++ scripts/env.js
@@ -2,7 +2,8 @@ import {execSync} from 'child_process';
 import {writeFileSync} from 'fs';
 
 const commitHash = execSync('git rev-parse HEAD').toString().trim();
+const msg = execSync('git log -1 --pretty=%B').toString().trim();
 const time = Date.now() / 1000;
 
 // write the commit hash to a .env file
-writeFileSync('.env', `COMMIT_HASH=${commitHash}\nCOMMIT_TIME=${time}\n`);
+writeFileSync('.env', `COMMIT_HASH=${commitHash}\nCOMMIT_MSG=${msg}\nCOMMIT_TIME=${time}\n`);
