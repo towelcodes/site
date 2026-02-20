9718658b8e38492218ed9097407486a678acd454
teatowel <tea@towel.codes>
2026-02-20 14:32:20 +0000

    hide counter in dev

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,5 +1,5 @@
 interface ImportMetaEnv {
-    readonly COMMIT_HASH: string,
-    readonly COMMIT_HASH_FULL: string,
-
+  readonly COMMIT_HASH: string;
+  readonly COMMIT_HASH_FULL: string;
+  readonly PROD: bool = true;
 }
