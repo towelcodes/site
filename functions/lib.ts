4313b7dbce257a30a71df8bc0878ed023383ef55
teatowel <tea@towel.codes>
2026-05-21 14:52:37 +0100

    tidy

---
+++ functions/lib.ts
@@ -0,0 +1,53 @@
+interface CacheResponse {
+  age: number;
+  data: string;
+  success: boolean;
+}
+
+export async function cacheOrFetch(
+  url: string,
+  options: RequestInit<RequestInitCfProperties>,
+  cache: KVNamespace<string>,
+  maxAge: number,
+): Promise<CacheResponse> {
+  // check cache
+  const cached_response = await cache.get(url);
+  if (cached_response) {
+    // check if response has expired
+    const expiry = await cache.get("expiry " + url);
+    if (expiry) {
+      if (parseInt(expiry) - Date.now() / 1000 > 0) {
+        const created = parseInt(expiry) - maxAge;
+        const age = Date.now() / 1000 - created;
+
+        return {
+          age,
+          data: cached_response,
+          success: true,
+        };
+      }
+    }
+  }
+
+  // cache miss
+  const res = await fetch(url, options);
+  if (res.status != 200) {
+    console.error(res);
+    return {
+      age: 0,
+      success: false,
+      data: await res.text(),
+    };
+  }
+
+  // store response
+  const body = await res.text();
+  await cache.put(url, JSON.stringify(body));
+  await cache.put("expiry " + url, `${Date.now() / 1000 + maxAge}`);
+
+  return {
+    success: true,
+    age: 0,
+    data: body,
+  };
+}
--- functions/songs.ts
+++ functions/songs.ts
@@ -2,7 +2,7 @@ const USER_ID = "teatowel";
 const BASE_URL = "https://api.listenbrainz.org";
 
 export const onRequest: PagesFunction<Env> = async (context) => {
-  const maxAge = 120;
+  const maxAge = 3600;
   const apikey = context.env.LISTENBRAINZ_KEY;
   const url = `${BASE_URL}/1/stats/user/${USER_ID}/recordings?range=week&count=5`;
 
