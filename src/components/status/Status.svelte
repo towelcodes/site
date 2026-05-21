3f12c1c8694e70303d639139c1fe5e3d4a1907c3
teatowel <tea@towel.codes>
2026-05-21 10:42:21 +0100

    use svelte transitions

--- src/components/status/Status.svelte
+++ src/components/status/Status.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import { onMount } from "svelte";
+    import { blur, fade } from "svelte/transition";
     import {
         defaultLanyardResponse,
         type Activity,
@@ -36,14 +37,11 @@
     }
 
     let presence_data = $state(defaultLanyardResponse);
-    $inspect(presence_data);
     let online_status = $derived(presence_data.discord_status);
     let online = $derived(online_status !== "offline");
     let activites: Activity[] | undefined = $derived(presence_data.activities);
     let listening_to_spotify = $derived(presence_data.listening_to_spotify);
     let spotify = $derived(presence_data.spotify);
-
-    let root: HTMLDivElement | undefined = $state();
 
     /**
      * TODO: use websocket
@@ -53,10 +51,6 @@
 
         // connect to the websocket
         const socket = new WebSocket("wss://api.lanyard.rest/socket");
-        let heartbeat_task: NodeJS.Timeout | undefined;
-        socket.addEventListener("open", (e) => {
-            console.info("socket opened");
-        });
         socket.addEventListener("message", (e) => {
             const data = JSON.parse(e.data) as WebsocketData;
             switch (data.op) {
@@ -77,7 +71,7 @@
                         }),
                     );
                     const d = data.d as WebsocketHello;
-                    heartbeat_task = setInterval(
+                    setInterval(
                         () => socket.send(JSON.stringify({ op: 3 })),
                         d.heartbeat_interval,
                     );
@@ -87,49 +81,39 @@
             }
         });
     });
-
-    $effect(() => {
-        if (root) {
-            const revealKeyframes = [
-                {
-                    opacity: "0",
-                },
-                {
-                    opacity: "1",
-                },
-            ];
-            root.animate(revealKeyframes, {
-                duration: 200,
-                fill: "forwards",
-            }).play();
-        }
-    });
 </script>
 
 <div class="flex flex-col gap-2 font-pixel">
-    <div class="w-min px-2 flex">
-        <svg
-            class="{online
-                ? 'fill-ctp-green'
-                : 'fill-ctp-surface0'} w-3 h-3 self-center mr-2 transition-colors"
-            viewBox="0 0 12 12"
-            xmlns="http://www.w3.org/2000/svg"
-        >
-            <circle cx="6" cy="6" r="6" />
-        </svg>
-        {online_status}
-    </div>
+    {#key online_status}
+        <div class="w-min px-2 flex">
+            <svg
+                class="{online
+                    ? 'fill-ctp-green'
+                    : 'fill-ctp-surface0'} w-3 h-3 self-center mr-2 transition-colors"
+                viewBox="0 0 12 12"
+                xmlns="http://www.w3.org/2000/svg"
+            >
+                <circle cx="6" cy="6" r="6" />
+            </svg>
+            {online_status}
+        </div>
+    {/key}
 
     {#if listening_to_spotify}
-        <div>
+        <div transition:fade={{ duration: 200 }}>
             {#if spotify}
                 <div class="relative">
                     <div class="text-ctp-subtext0">listening to</div>
-                    <div class="relative left-5">
-                        {spotify!.song}
-                        <span class="text-ctp-subtext0">-</span>
-                        {spotify!.artist}
-                    </div>
+                    {#key spotify.song}
+                        <div
+                            class="relative left-5"
+                            transition:blur={{ duration: 200 }}
+                        >
+                            {spotify!.song}
+                            <span class="text-ctp-subtext0">-</span>
+                            {spotify!.artist}
+                        </div>
+                    {/key}
                 </div>
             {/if}
         </div>
