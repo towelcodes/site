14d6ab0455fd33fdfa403b501491416aa00c5a84
teatowel <tea@towel.codes>
2026-08-02 00:15:32 +0100

    change title to svelte component

---
+++ src/components/TitleNav.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import Teatowel from "../assets/typography/teatowels.svg?raw";
+    import Home from "../assets/typography/home.svg?raw";
+    let { path = "" } = $props();
+</script>
+
+<div class="flex gap-2 *:hover:cursor-default *:block items-center">
+    <div class="h-[36px] relative">
+        {@html Teatowel}
+    </div>
+    <div class="h-[36px] relative drop-shadow-glow drop-shadow-ctp-green">
+        {@html Home}
+    </div>
+</div>
